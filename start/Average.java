4ff37b79e91ffec4d58805434f5108d4eaf9cd86
Rajkumar <maskarrajkumar@gmail.com>
2022-09-21 17:04:56 +0530

    Second Program

---
+++ start/Average.java
@@ -0,0 +1,27 @@
+package start;
+import java.util.Scanner;
+public class Average {
+
+	public static void main(String[] args) {
+		//Scanner s = new Scanner(System.in);
+		
+		//String str = s.next();
+       // char ch = str.charAt(0);
+      //  int a = s.nextInt();
+     //   int b = s.nextInt();
+       // int c = s.nextInt();
+        //System.out.println(ch);
+        //System.out.print(a + " " + b);
+        //System.out.println(" " + c);
+        //System.out.println(ch);
+        //System.out.println((a + b + c)/3);
+		int i = 'c';
+		System.out.println(i);
+
+
+
+
+	
+	}
+
+}
